6e6f2e6c82f234b9dde526d2fa97259edb38634b
Melina Nadal <melinanhopkins@gmail.com>
2020-12-14 17:41:45 -0500

    cleans up files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 require('dotenv').config();
 
-const scoresRouter = require('./routes/api/scores');
+
 const usersRouter = require('./routes/api/users');
 
 
@@ -18,14 +18,8 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/api/scores', scoresRouter);
 app.use('/api/users', usersRouter);
 
-
-app.use('/api/users', require('./routes/api/users'));
-// Mount our custom auth middleware to protect routes below it
-app.use(require('./config/auth'));
-app.use('/api/scores', require('./routes/api/scores'));
 const port = process.env.PORT || 3001;
 	
 app.listen(port, function() {
